f62a02b8d942b194b20245873c5a70f3eacb86b4
Ivan-jauregui <162524712+Ivan-jauregui@users.noreply.github.com>
2024-04-24 19:50:48 -0300

    Add files via upload

---
+++ EjerciciosNodeJS/MatematicoCurioso/matematica.js
@@ -0,0 +1,97 @@
+const http = require('node:http')
+const fs = require('node:fs')
+
+const mime = {
+  'html': 'text/html',
+  'css': 'text/css',
+  'jpg': 'image/jpg',
+  'ico': 'image/x-icon',
+  'mp3': 'audio/mpeg3',
+  'mp4': 'video/mp4'
+}
+
+const servidor = http.createServer((pedido, respuesta) => {
+  const url = new URL('http://localhost:8888' + pedido.url)
+  let camino = 'public' + url.pathname
+  if (camino == 'public/')
+    camino = 'public/index.html'
+  encaminar(pedido, respuesta, camino)
+})
+
+servidor.listen(8888)
+
+
+function encaminar(pedido, respuesta, camino) {
+  console.log(camino)
+  switch (camino) {
+    case 'public/recuperardatos': {
+      recuperar(pedido, respuesta)
+      break
+    }
+    default: {
+      fs.stat(camino, error => {
+        if (!error) {
+          fs.readFile(camino, (error, contenido) => {
+            if (error) {
+              respuesta.writeHead(500, { 'Content-Type': 'text/plain' })
+              respuesta.write('Error interno')
+              respuesta.end()
+            } else {
+              const vec = camino.split('.')
+              const extension = vec[vec.length - 1]
+              const mimearchivo = mime[extension]
+              respuesta.writeHead(200, { 'Content-Type': mimearchivo })
+              respuesta.write(contenido)
+              respuesta.end()
+            }
+          })
+        } else {
+          respuesta.writeHead(404, { 'Content-Type': 'text/html' })
+          respuesta.write('<!doctype html><html><head></head><body>Recurso inexistente</body></html>')
+          respuesta.end()
+        }
+      })
+    }
+  }
+}
+
+
+function recuperar(pedido, respuesta) {
+  let info = ''
+  pedido.on('data', datosparciales => {
+    info += datosparciales
+  })
+  pedido.on('end', () => {
+    const formulario = new URLSearchParams(info)
+    console.log(formulario)
+    respuesta.writeHead(200, { 'Content-Type': 'text/html' })
+
+    let S= parseInt(formulario.get('semilla'));
+    let secuencia=S
+    secuenciaTotal=[];
+
+    secuenciaTotal.push(secuencia);
+
+    while (secuencia !== 1){
+        if(secuencia % 2 == 0){
+            secuencia /= 2
+        }
+        else{
+            secuencia=(secuencia*3)+1
+        }
+        secuenciaTotal.push(secuencia); 
+    }
+
+    const pagina =
+      `<!doctype html><html><head></head><body>
+     La semilla es: ${formulario.get('semilla')}<br>
+     La secuencia es: ${secuenciaTotal}<br>
+     la cantidad de elementos de la secuencia es: ${secuenciaTotal.length}<br>
+     El numeor mas grande de la secuencia es: ${(Math.max(...secuenciaTotal))}
+     <a href="index.html">Retornar</a>
+     </body></html>`
+    respuesta.end(pagina)
+  })
+}
+
+console.log('Servidor web iniciado')
